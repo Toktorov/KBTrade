123106fb4f3da37dfd4b4e5e682a6a69322f35b2
Toktorov <ktoktorov2005@gmail.com>
2023-09-15 21:21:53 +0600

    some changes

--- core/settings.py
+++ core/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_USER_PASSWORD"),
-        'HOST': 'localhost',
+        'HOST': os.environ.get("DB_HOST"),
         'PORT': os.environ.get("DB_PORT"),
     }
 }
